927a310d462d7e6fd767585a56469b6c78ec565b
Orivaldo Eder Matavelli <eder.matavelli@gmail.com>
2026-05-12 21:33:54 -0300

    Criação
    Criado o arquivo Teste

---
+++ cadastrar.py
@@ -0,0 +1,5 @@
+def cadastrar_aluno():
+    nome = input("Digite o nome do aluno: ")
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    return nome, nota1, nota2
